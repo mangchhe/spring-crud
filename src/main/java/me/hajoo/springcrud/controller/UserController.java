9f217dc31275a8a1b1e39515f1959ee3c8452df6
joohyun ha <mylifeforcoding@gmail.com>
2022-03-20 14:54:52 +0900

    docs: swagger 추가

--- src/main/java/me/hajoo/springcrud/controller/UserController.java
+++ src/main/java/me/hajoo/springcrud/controller/UserController.java
@@ -1,5 +1,7 @@
 package me.hajoo.springcrud.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import me.hajoo.springcrud.dto.CreateUserRequest;
 import me.hajoo.springcrud.dto.UpdateUserRequest;
@@ -18,24 +20,28 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "회원가입")
     @PostMapping("")
     public ResponseEntity<String> createUser(@RequestBody @Valid final CreateUserRequest createUserRequest) {
         userService.createUser(createUserRequest);
         return ResponseEntity.ok("");
     }
 
+    @Operation(summary = "모든 유저 조회", description = "모든 유저 조회")
     @GetMapping("")
     public ResponseEntity<List<UserResponse>> findUsers() {
         final List<UserResponse> body = userService.findUsers();
         return ResponseEntity.ok(body);
     }
 
+    @Operation(summary = "프로필 수정", description = "프로필 수정")
     @PostMapping("/profile")
     public ResponseEntity<String> updateUser(@RequestBody @Valid final UpdateUserRequest updateUserRequest) {
         userService.updateUser(updateUserRequest);
         return ResponseEntity.ok("");
     }
 
+    @Operation(summary = "회원탈퇴", description = "회원탈퇴")
     @DeleteMapping("/{userId}")
     public ResponseEntity<String> deleteUser(@PathVariable final Long userId) {
         userService.deleteUser(userId);
--- src/main/java/me/hajoo/springcrud/dto/CreateUserRequest.java
+++ src/main/java/me/hajoo/springcrud/dto/CreateUserRequest.java
@@ -1,5 +1,6 @@
 package me.hajoo.springcrud.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,15 +10,22 @@ import javax.validation.constraints.*;
 @RequiredArgsConstructor
 public class CreateUserRequest {
 
+    @Schema(required = true, defaultValue = "홍길동", description = "이름")
     @NotBlank
     @Size(min = 2, max = 255)
     private final String name;
+
+    @Schema(required = true, defaultValue = "27", minimum = "1", description = "나이")
     @NotNull
     @Positive(message = "나이는 1살 이상이어야 합니다.")
     private final Integer age;
+
+    @Schema(required = true, defaultValue = "184", minimum = "1", description = "키")
     @NotNull
     @Positive(message = "키는 1cm 이상이어야 합니다.")
     private final Integer height;
+
+    @Schema(required = true, defaultValue = "75", minimum = "1", description = "몸무게")
     @NotNull
     @PositiveOrZero(message = "몸무게는 1kg 이상이어야 합니다.")
     private final Integer weight;
